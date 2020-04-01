57e5c18cf1386645014b30bfd346278283aa5031
jja42 <jja42@njit.edu>
2020-04-01 01:41:54 -0400

    hitboxes and attack spawning

--- src/level.c
+++ src/level.c
@@ -7,19 +7,15 @@ void load_level_zubat(){
     Entity *platform_test2 = malloc(sizeof(Entity)); 
     platform_test2 = gf2d_entity_new();
     Enemy *enemy_test = malloc(sizeof(Enemy)); 
-    //load_pikachu(enemy_test);
-    //load_articuno(enemy_test);
-    //load_zubat(enemy_test);
-    //load_betamon(enemy_test,vector2d(750,250),740,800);
-    //load_penguinmon(enemy_test,vector2d(750,250),740,800);
+    
     load_wormmon(enemy_test,vector2d(750,200),vector2d(1,0));
     
-    gf2d_entity_load(platform_test,"images/box.png",40,40,1,vector2d(550,560),vector2d(3,3));
-    gf2d_entity_load(platform_test2,"images/box.png",40,40,1,vector2d(750,400),vector2d(3,3));
-    platform_test->box = gf2d_box(platform_test->position, 60, 60, vector2d(60,60));
+    gf2d_entity_load(platform_test,"images/platform.png",48,16,1,vector2d(550,560),vector2d(3,3));
+    gf2d_entity_load(platform_test2,"images/platform.png",48,16,1,vector2d(750,400),vector2d(3,3));
+    platform_test->box = gf2d_box(platform_test->position, 72, 24, vector2d(72,24));
     platform_test->touch = platform_touch;
     platform_test->tag = 6;
-    platform_test2->box = gf2d_box(platform_test2->position, 60, 60, vector2d(60,60));
+    platform_test2->box = gf2d_box(platform_test2->position, 72, 24, vector2d(72,24));
     platform_test2->touch = platform_touch;
     platform_test2->tag = 6;
 }
